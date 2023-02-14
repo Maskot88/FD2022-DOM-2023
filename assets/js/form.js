f31c13ed20716e372575bac147a3b49cc3feea8d
Lymarenko Dmytro <benimaru046@gmail.com>
2023-02-14 15:22:20 +0100

    Add fetch & then

---
+++ assets/js/form.js
@@ -0,0 +1,45 @@
+"use strict";
+
+// const patternFullName = /^([A-Z][a-z]+( [A-Z][a-z]+))$/;
+// const patternFullName = /^[A-Z][a-z]{2,15} [A-Z][a-z]{2,15}$/;
+
+const logins = [];
+const list = document.getElementById("list"); //render
+const form = document.getElementById("form");
+const login = form.login;
+login.focus();
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const {
+    target,
+    target: { login },
+  } = event;
+  const newLogin = login.value.trim();
+  if (newLogin && logins.includes(newLogin) === false) {
+    logins.push(newLogin);
+    list.append(createLiLogin(newLogin));
+  }
+  target.reset();
+});
+
+function createLiLogin(newLogin) {
+  return createElement(
+    "li",
+    {},
+    createElement("span", {}, newLogin),
+    createElement(
+      "button",
+      {
+        styles: { color: "red", fontWeight: "bolder" },
+        listeners: {
+          click: ({ target: { parentElement, previousElementSibling } }) => {
+            parentElement.remove();
+            logins.splice(logins.indexOf(previousElementSibling.innerText), 1);
+          },
+        },
+      },
+      "X"
+    )
+  );
+}
--- index.js
+++ index.js
@@ -1,3 +1,34 @@
 "use strict";
 
-const form = document.getElementById("form");
+const promise = fetch("./assets/js/data.json").then((response) =>
+  response.json().then((data) => {
+    console.table(data);
+  })
+);
+// console.log(promise);
+
+// promise.then(
+//   (response) => {
+//     const responsePromise = response.json();
+//     console.log("ok", responsePromise);
+//     responsePromise.then(
+//       (data) => {
+//         console.table(data);
+//       },
+//       () => {
+//         console.log('error responsrPromise')
+//       }
+//     );
+//   },
+//   () => {
+//     console.log("error promise");
+//   }
+// );
+
+// const responsrPromise = promise.then((response) => {
+//   return response.json();
+// });
+
+// responsrPromise.then((data) => {
+//   console.table(data);
+// });

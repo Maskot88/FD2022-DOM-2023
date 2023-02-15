e96526ee5b240a148fe7765817e7acb5d3bbfc21
Lymarenko Dmytro <benimaru046@gmail.com>
2023-02-15 13:48:17 +0100

    Add loadImage

--- index.js
+++ index.js
@@ -1,34 +1,43 @@
 "use strict";
 
-const promise = fetch("./assets/js/data.json").then((response) =>
-  response.json().then((data) => {
-    console.table(data);
+const promise = new Promise((resolve, reject) => {
+  Math.random() > 0.5 ? resolve("data") : reject("error");
+});
+
+promise
+  .then((data) => {
+    console.log(data);
   })
-);
-// console.log(promise);
+  .catch((error) => {
+    console.log(error);
+  });
 
-// promise.then(
-//   (response) => {
-//     const responsePromise = response.json();
-//     console.log("ok", responsePromise);
-//     responsePromise.then(
-//       (data) => {
-//         console.table(data);
-//       },
-//       () => {
-//         console.log('error responsrPromise')
-//       }
-//     );
-//   },
-//   () => {
-//     console.log("error promise");
-//   }
-// );
+function timeout(time, word) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(word);
+    }, time);
+  });
+}
 
-// const responsrPromise = promise.then((response) => {
-//   return response.json();
-// });
+timeout(1500, "wordqwewqe").then((data) => {
+  console.log(data);
+});
 
-// responsrPromise.then((data) => {
-//   console.table(data);
-// });
+function loadImage(path) {
+  const newImage = new Image();
+  newImage.src = path;
+  return new Promise((resolve, reject) => {
+    newImage.addEventListener("load", () => {
+      resolve(newImage);
+    });
+    newImage.addEventListener("error", () => {
+      reject(new Error("path invalid"));
+    });
+  });
+}
+loadImage('https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/Paracas_National_Reserve%2C_Ica%2C_Peru-3April2011.jpg/1200px-Paracas_National_Reserve%2C_Ica%2C_Peru-3April2011.jpg')
+  .then((elem) => {
+    document.body.append(elem);
+  })
+  .catch((error) => console.log(error));
